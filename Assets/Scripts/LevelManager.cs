f07757207d3edebedf1543423d6e7f31b9327aae
SzymonK95 <szymon_kaszuba97@wp.pl>
2018-05-10 22:13:57 +0200

    Finish section 5 lecture 71

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
@@ -7,7 +8,7 @@ public class LevelManager : MonoBehaviour
     public void LoadLevel(string name)
     {
         Debug.Log("New Level load: " + name);
-        Application.LoadLevel(name);
+        SceneManager.LoadScene(name);
     }
 
     public void QuitRequest()
