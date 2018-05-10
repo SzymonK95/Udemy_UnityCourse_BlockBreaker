8c39e9b3de6b27512411a533659995fff1b4d73c
SzymonK95 <szymon_kaszuba97@wp.pl>
2018-05-10 23:09:40 +0200

    Finish section 5 lecture 75

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour
+{
+
+    public LevelManager LevelManager;
+
+    void OnTriggerEnter2D (Collider2D trigger)
+    {
+        LevelManager.LoadLevel("Win Screen");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+    }
+}
