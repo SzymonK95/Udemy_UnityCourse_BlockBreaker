9056e1f58b14f2b05c2d13776e5d6403c9faf7d0
SzymonK95 <szymon_kaszuba97@wp.pl>
2018-05-11 07:06:05 +0200

    Finish section 5 lecture 78

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    private float mousePositionBlocksX;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 paddlePos = new Vector3(1f,this.transform.position.y,this.transform.position.z);
+
+	    mousePositionBlocksX = Input.mousePosition.x / Screen.width * 16;
+
+	    paddlePos.x = Mathf.Clamp(mousePositionBlocksX,0.5f,15.5f);
+
+	    this.transform.position = paddlePos;
+	}
+}
